59808007130f16ff6b530eb7ff5495c1b2185590
sameerasw <sameera.whoami@gmail.com>
2024-12-25 22:13:33 +0530

    Initial commit

---
+++ src/main/java/com/sameerasw/mycart/MycartApplication.java
@@ -0,0 +1,13 @@
+package com.sameerasw.mycart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycartApplication.class, args);
+	}
+
+}
